c437e7c4976a39b648ad5c06edfe831a5c526395
etWert <i6725155@gmail.com>
2025-05-29 11:17:23 +0300

    Add Mongoose schema for Tool model

---
+++ backend/models/Tool.js
@@ -0,0 +1,19 @@
+const mongoose = reqiure('mongoose')
+const toolSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description:String,
+    price: {
+        type: Number,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+module.exports=mongoose.model('Tool',toolSchema)
