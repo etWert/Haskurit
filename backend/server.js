a38fb918c9ab1e6f40fade65cce502f4842fa5fb
etWert <i6725155@gmail.com>
2025-05-29 14:05:25 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,20 @@
 require("dotenv").config()
-const express=require("express")
-const PORT=process.eventNames.PORT||1000
+const express = require("express")
+const cors = require("cors")
+const corsOptions = require("./config/corsOptions")
+const connectDB = require("./config/connectDB")
+
+const app = express()
+const PORT = process.env.PORT || 1000
+
+app.use(cors(corsOptions))
+app.use(express.json())
+// app.use(express.static("public"))
+
+connectDB()
+
+app.use("/tools/", require("./routes/ToolRoute"))
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+})
