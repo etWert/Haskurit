230f7c006c68f55f95d55ba4513caddd53ce11a1
etWert <i6725155@gmail.com>
2025-06-10 18:35:39 +0300

    add middlewares verifyJWT and verifyAdmin

---
+++ backend/middleware/verifyAdmin.js
@@ -0,0 +1,13 @@
+const verifyAdmin = (req, res, next) => {
+    if (req.user && (req, user.role === "admin")) {
+        next()
+    }
+    else {
+        return res.status(401).json({
+            error: true,
+            message: "Not admin",
+            data: null
+        })
+    }
+}
+module.exports = verifyAdmin
