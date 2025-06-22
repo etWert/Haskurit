2937861791b1651011aa9c01073713c7696546a4
etWert <i6725155@gmail.com>
2025-06-22 17:03:39 +0300

    add tailwindCSS and install packages

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'haskurit-yellow': '#FFC708',
+        'haskurit-gray': '#5A5A5C',
+        'haskurit-white': '#FFFFFF'
+      }
+    },
+  },
+  plugins: [],
+}
