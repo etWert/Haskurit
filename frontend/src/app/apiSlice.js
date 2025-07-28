a0806aae42e9eafb94f0e74b679bb2a88a1fbf76
etWert <i6725155@gmail.com>
2025-07-28 09:24:04 +0300

    Update apiSlice

--- frontend/src/app/apiSlice.js
+++ frontend/src/app/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setToken, logout } from '../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'https://haskurit.onrender.com/api',
+  baseUrl: process.env.REACT_APP_API_URL + '/api' || process.env.REACT_APP_API_URL_LOCAL + '/api',
   credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token
