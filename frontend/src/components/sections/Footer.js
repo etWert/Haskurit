59eb0607392ce2da784ea0bd5841d63b9d6b0cab
etWert <i6725155@gmail.com>
2025-08-01 14:51:49 +0300

    Update Footer component

--- frontend/src/components/sections/Footer.js
+++ frontend/src/components/sections/Footer.js
@@ -1,8 +1,19 @@
 const Footer = () => {
-    return  <footer className="bg-[#5A5A5C] text-white py-6 text-center">
-      <p>פותח ע"י איטי דונט עבור השכורית</p>
-      <a href="https://github.com/etwert" target="_blank" rel="noopener noreferrer" className="underline">GitHub</a>
+  return (
+    <footer className="bg-[#5A5A5C] text-white py-6 text-center">
+      <p>
+        © פותח ע"י{' '}
+        <a 
+          href="https://https://dev-portfolio-iti-donat.netlify.app" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="underline hover:text-blue-400 transition"
+        >
+          איטי דונט
+        </a>{' '}
+        עבור השכורית
+      </p>
     </footer>
- 
+  );
 }
-export default Footer
+export default Footer;
