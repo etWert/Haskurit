0eff89b56e68dea1bfe5202e7d9ab2e926e58989
etWert <i6725155@gmail.com>
2025-08-03 17:26:18 +0300

    Update HomePage and delete Hero component

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,14 +1,16 @@
-import Hero from '../components/sections/Hero'
+// import Hero from '../components/sections/Hero'
 import Tools from '../components/sections/Tools'
 import Contact from '../components/sections/Contact'
 import Footer from '../components/sections/Footer'
+import Banner from '../components/sections/Banner'
+import About from '../components/sections/About'
 
 const HomePage = () => {
     return (
         <div className="min-h-screen bg-haskurit-white">
-            <Hero />
+            <Banner />
+            <About />
             <Tools />
-            {/* <About /> */}
             <Contact />
             <Footer />
         </div>
