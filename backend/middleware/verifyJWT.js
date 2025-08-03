a287ced6a753085a13216a0d3fe6748a52cbbd68
etWert <i6725155@gmail.com>
2025-08-03 17:29:44 +0300

    Update VerifyJWT

--- backend/middleware/verifyJWT.js
+++ backend/middleware/verifyJWT.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken")
 const verifyJWT = (req, res, next) => {
-    const authHeader = req.headers.authorization || req.headers.authorization
+    const authHeader = req.headers.authorization || req.headers.Authorization
     if (!authHeader?.startsWith("Bearer ")) {
         return res.status(401).json({
             error: true,
