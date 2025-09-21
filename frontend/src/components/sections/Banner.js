01d6d55efb3c539c6bef144b4235fa2783ef9a03
etWert <i6725155@gmail.com>
2025-09-21 18:14:26 +0300

    try image size

--- frontend/src/components/sections/Banner.js
+++ frontend/src/components/sections/Banner.js
@@ -2,14 +2,11 @@ import { HiOutlineArrowDown } from "react-icons/hi";
 
 const Banner = () => {
     return (
-        <div className="w-full h-screen flex flex-col">
-            {/* באנר שמתאים לתמונה */}
-            <div className="relative w-full overflow-hidden shadow-lg">
-                <picture>
+        <picture>
                     {/* תמונה לדסקטופ */}
                     <source
                         media="(min-width: 768px)"
-                        srcSet="/images/banner-narrower.jpg"
+                        srcSet="/images/למחשב.png"
                     />
                     {/* תמונה למובייל */}
                     <img
@@ -18,14 +15,30 @@ const Banner = () => {
                         className="w-full h-auto"
                     />
                 </picture>
-            </div>
-            <div className="flex-1 flex items-center justify-end px-8 md:px-16">
-                <HiOutlineArrowDown
-                    className="animate-bounce text-4xl text-haskurit-gray mx-auto cursor-pointer"
-                    onClick={() => document.getElementById('about').scrollIntoView({ behavior: 'smooth' })}
-                />
-            </div>
-        </div>
+        // <div className="w-full h-screen flex flex-col">
+        //     {/* באנר שמתאים לתמונה */}
+        //     <div className="relative w-full overflow-hidden shadow-lg">
+        //         <picture>
+        //             {/* תמונה לדסקטופ */}
+        //             <source
+        //                 media="(min-width: 768px)"
+        //                 srcSet="/images/למחשב.png"
+        //             />
+        //             {/* תמונה למובייל */}
+        //             <img
+        //                 src="/images/logo.jpg"
+        //                 alt="באנר ראשי"
+        //                 className="w-full h-auto"
+        //             />
+        //         </picture>
+        //     </div>
+        //     <div className="flex-1 flex items-center justify-end px-8 md:px-16">
+        //         <HiOutlineArrowDown
+        //             className="animate-bounce text-4xl text-haskurit-gray mx-auto cursor-pointer"
+        //             onClick={() => document.getElementById('about').scrollIntoView({ behavior: 'smooth' })}
+        //         />
+        //     </div>
+        // </div>
     );
 }
 
