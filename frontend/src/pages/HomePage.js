66d87e6103dd44b5e15437f21781d914ea037644
etWert <i6725155@gmail.com>
2025-11-23 22:48:46 +0200

    Update HomePage component

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,14 +1,17 @@
-// import Hero from '../components/sections/Hero'
+import Banner from '../components/sections/Banner'
+import Menu from '../components/sections/Menu'
+import About from '../components/sections/About'
 import Tools from '../components/sections/Tools'
 import Contact from '../components/sections/Contact'
 import Footer from '../components/sections/Footer'
-import Banner from '../components/sections/Banner'
-import About from '../components/sections/About'
 
 const HomePage = () => {
     return (
         <div className="min-h-screen bg-haskurit-white">
             <Banner />
+            <div className="absolute top-0 left-0 right-0 z-10 pt-4 flex justify-start pr-8">
+                <Menu />
+            </div>
             <About />
             <Tools />
             <Contact />
