6405c44ab0dd1ddbacb865d3aace528c050e8a86
etWert <i6725155@gmail.com>
2025-11-23 22:33:37 +0200

    Update Footer component

--- frontend/src/components/sections/Footer.js
+++ frontend/src/components/sections/Footer.js
@@ -3,14 +3,10 @@ const Footer = () => {
     <footer className="bg-[#5A5A5C] text-white py-6 text-center">
       <p>
         © פותח ע"י{' '}
-        <a 
-          href="https://https://dev-portfolio-iti-donat.netlify.app" 
-          target="_blank" 
-          rel="noopener noreferrer" 
-          className="underline hover:text-blue-400 transition"
-        >
+        <span>
           איטי דונט
-        </a>{' '}
+        </span>
+        {' '}
         עבור השכורית
       </p>
     </footer>
