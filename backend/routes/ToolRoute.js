0ec7367224a7060d6f66af3ff28d32715acd740b
etWert <etdonat@gmail.com>
2025-12-29 23:21:06 +0200

    Update Tool route

--- backend/routes/ToolRoute.js
+++ backend/routes/ToolRoute.js
@@ -1,22 +1,15 @@
 const express = require("express")
 const ToolController = require("../controllers/ToolController")
-const { upload } = require("../config/cloudinary")
+const { toolUpload } = require("../config/cloudinary")
 const handleUploadErrors = require("../middleware/handleUploadErrors")
 const verifyJWT = require("../middleware/verifyJWT")
 const verifyAdmin = require("../middleware/verifyAdmin")
 const router = express.Router()
 
-// GET routes
 router.get("/", ToolController.getAllTools)
 router.get("/:id", ToolController.getToolById)
-
-// POST route - עם העלאת תמונה
-router.post("/", verifyJWT, verifyAdmin, upload.single('image'), handleUploadErrors, ToolController.createTool)
-
-// PUT route - עם העלאת תמונה אופציונלית  
-router.put("/", verifyJWT, verifyAdmin, upload.single('image'), handleUploadErrors, ToolController.updateTool)
-
-// DELETE route
+router.post("/", verifyJWT, verifyAdmin, toolUpload.single('image'), handleUploadErrors, ToolController.createTool)
+router.put("/", verifyJWT, verifyAdmin, toolUpload.single('image'), handleUploadErrors, ToolController.updateTool)
 router.delete("/", verifyJWT, verifyAdmin, ToolController.deleteTool)
 
 module.exports = router
